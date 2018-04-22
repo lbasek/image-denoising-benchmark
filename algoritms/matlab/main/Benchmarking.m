fb353e85dd7672f7c841ba9e7b36fcf696ab6f9f
Luka <luka.basek@gmail.com>
2018-04-22 22:44:05 +0200

    Implemented benchmarking

--- algoritms/matlab/main/Benchmarking.m
+++ algoritms/matlab/main/Benchmarking.m
@@ -8,18 +8,15 @@ addpath(genpath('../PSNR'));
 addpath(genpath('../MSE'));
 addpath(genpath('../SSIM'));
 
-BM3D = 1;
-KSVD = 1;
-WNNM = 0;
-FOE  = 0;
-NCSR = 0;
-EPLL = 0;
-
+IS_INCLUDED_BM3D = 1;
+IS_INCLUDED_KSVD = 1;
+IS_INCLUDED_WNNM = 1;
+IS_INCLUDED_FOE  = 1;
+IS_INCLUDED_NCSR = 1;
+IS_INCLUDED_EPLL = 1;
 
 % SIGMA [ 10, 15, 20, 25, 50 ]
-SIGMA =[10];
-
-% time TODO
+SIGMA =[ 10, 15, 20, 25, 50];
 
 for s = SIGMA
     
@@ -35,8 +32,24 @@ for s = SIGMA
     ksvd_psnr  = [];
     ksvd_ssim  = [];
     
+    wnnm_mse   = [];
+    wnnm_psnr  = [];
+    wnnm_ssim  = [];
+    
+    foe_mse   = [];
+    foe_psnr  = [];
+    foe_ssim  = [];
+    
+    ncsr_mse   = [];
+    ncsr_psnr  = [];
+    ncsr_ssim  = [];
+    
+    epll_mse   = [];
+    epll_psnr  = [];
+    epll_ssim  = [];
+    
     %images
-    for i = 1:2
+    for i = 1:20
         
         BatchPath = strcat('../../../dataset/batch',int2str(i));
 
@@ -68,7 +81,7 @@ for s = SIGMA
         %--------------------------------------------------------------------------------------------------------
         
         % BM3D
-        if BM3D == 1
+        if IS_INCLUDED_BM3D == 1
             Denoised_Image_BM3D = BM3D(NOISY,s);
             mse  =  MSE(Reference_Image,Clean_Image,Denoised_Image_BM3D);
             psnr =  PSNR(Reference_Image,Clean_Image,Denoised_Image_BM3D);
@@ -86,7 +99,7 @@ for s = SIGMA
         %--------------------------------------------------------------------------------------------------------
 
         % KSVD
-        if KSVD == 1
+        if IS_INCLUDED_KSVD == 1
             Denoised_Image_KSVD = KSVD_WRAP(NOISY,s);
             mse  =   MSE(Reference_Image,Clean_Image,Denoised_Image_KSVD);
             psnr =  PSNR(Reference_Image,Clean_Image,Denoised_Image_KSVD);
@@ -103,12 +116,16 @@ for s = SIGMA
         %--------------------------------------------------------------------------------------------------------
 
         % WNNM
-        if WNNM == 1
+        if IS_INCLUDED_WNNM == 1
             Denoised_Image_WNNM = WNNM_WRAP(NOISY,s);
             mse  =   MSE(Reference_Image,Clean_Image,Denoised_Image_WNNM);
             psnr =  PSNR(Reference_Image,Clean_Image,Denoised_Image_WNNM);
             ssim =  SSIM(Reference_Image,Clean_Image,Denoised_Image_WNNM);
             sprintf('After WNNM denoising: MSE= %g, PSNR= %g, SSIM= %g',mse, psnr, ssim);
+            
+            wnnm_mse  = [wnnm_mse mse];
+            wnnm_psnr = [wnnm_psnr psnr];
+            wnnm_ssim = [wnnm_ssim ssim];
 
             row = cell2table({'WNNM',mse,psnr,ssim});
             tableResults = [tableResults ; row];
@@ -116,12 +133,16 @@ for s = SIGMA
         %--------------------------------------------------------------------------------------------------------
 
         % FOE
-        if FOE == 1
+        if IS_INCLUDED_FOE == 1
             Denoised_Image_FOE = FOE_WRAP(NOISY, s);
             mse  =   MSE(Reference_Image,Clean_Image,Denoised_Image_FOE);
             psnr =  PSNR(Reference_Image,Clean_Image,Denoised_Image_FOE);
             ssim =  SSIM(Reference_Image,Clean_Image,Denoised_Image_FOE);
             sprintf('After FOE denoising: MSE= %g, PSNR= %g, SSIM= %g',mse, psnr, ssim);
+            
+            foe_mse  = [foe_mse mse];
+            foe_psnr = [foe_psnr psnr];
+            foe_ssim = [foe_ssim ssim];
 
             row = cell2table({'FOE',mse,psnr,ssim});
             tableResults = [tableResults ; row];
@@ -130,12 +151,16 @@ for s = SIGMA
         %--------------------------------------------------------------------------------------------------------
 
         % NCSR
-        if NCSR == 1
+        if IS_INCLUDED_NCSR == 1
             Denoised_Image_NCSR = NCSR_WRAP(NOISY,s);
             mse  =   MSE(Reference_Image,Clean_Image,Denoised_Image_NCSR);
             psnr =  PSNR(Reference_Image,Clean_Image,Denoised_Image_NCSR);
             ssim =  SSIM(Reference_Image,Clean_Image,Denoised_Image_NCSR);
             sprintf('After NCSR denoising: MSE= %g, PSNR= %g, SSIM= %g',mse, psnr, ssim);
+            
+            ncsr_mse  = [ncsr_mse mse];
+            ncsr_psnr = [ncsr_psnr psnr];
+            ncsr_ssim = [ncsr_ssim ssim];
 
             row = cell2table({'NCSR',mse,psnr,ssim});
             tableResults = [tableResults ; row];
@@ -145,12 +170,16 @@ for s = SIGMA
 
                
         % EPLL
-        if EPLL == 1
+        if IS_INCLUDED_EPLL == 1
             Denoised_Image_EPLL = EPLL_WRAP(NOISY, s);
             mse  =   MSE(Reference_Image,Clean_Image,Denoised_Image_EPLL);
             psnr =  PSNR(Reference_Image,Clean_Image,Denoised_Image_EPLL);
             ssim =  SSIM(Reference_Image,Clean_Image,Denoised_Image_EPLL);
             sprintf('After EPLL denoising: MSE= %g, PSNR= %g, SSIM= %g',mse, psnr, ssim);
+            
+            epll_mse  = [epll_mse mse];
+            epll_psnr = [epll_psnr psnr];
+            epll_ssim = [epll_ssim ssim];
 
             row = cell2table({'EPLL',mse,psnr,ssim});
             tableResults = [tableResults ; row];
@@ -158,20 +187,31 @@ for s = SIGMA
 
         %--------------------------------------------------------------------------------------------------------
 
-        filename = strcat('results_sigma',int2str(s),'_image', int2str(i),'.txt');
+        filename = strcat('./results/','results_sigma',int2str(s),'_image', int2str(i),'.csv');
         tableResults.Properties.VariableNames = {'Algorithm' 'MSE' 'PSNR' 'SSIM'};
-        writetable(tableResults,filename, 'Delimiter',',')
+        writetable(tableResults,filename, 'Delimiter',',','QuoteStrings',true)
         type filename
     
     end
     
     finalResults = table();
-    finalResults.Properties.VariableNames = {'QUALITY-METRIC' 'ORIGINAL' 'BM3D' 'KSVD' 'WNNM' 'FOE' 'NCSR' 'EPLL'};
-    row = cell2table({'PSNR',mean(original_psnr),mean(bm3d_psnr),0,0,0,0,0});
+    
+    % MSE
+    row = cell2table({'MSE',mean(original_mse),mean(bm3d_mse),mean(ksvd_mse),mean(wnnm_mse),mean(foe_mse),mean(ncsr_mse),mean(epll_mse)});
     finalResults = [finalResults ; row];
     
-    filename = strcat('final_results_',int2str(s),'.txt');
-    writetable(finalResults,filename, 'Delimiter',',')
+    % PSNR
+    row = cell2table({'PSNR',mean(original_psnr),mean(bm3d_psnr),mean(ksvd_psnr),mean(wnnm_psnr),mean(foe_psnr),mean(ncsr_psnr),mean(epll_psnr)});
+    finalResults = [finalResults ; row];
+   
+    % SSIM
+    row = cell2table({'SSIM',mean(original_ssim),mean(bm3d_ssim),mean(ksvd_ssim),mean(wnnm_ssim),mean(foe_ssim),mean(ncsr_ssim),mean(epll_ssim)});
+    finalResults = [finalResults ; row];
+    
+    filename = strcat('./results/', 'final_results_sigma',int2str(s),'.csv');
+    finalResults.Properties.VariableNames = {'QM' 'ORIGINAL' 'BM3D' 'KSVD' 'WNNM' 'FOE' 'NCSR' 'EPLL'};
+    
+    writetable(finalResults,filename, 'Delimiter',',','QuoteStrings',true)
     type filename
 
 end   
